a076a140ad8885035ead648997cf12d2613b142f
rajkumarchintapalli <raj.chintapalli@zessta.com>
2022-04-08 12:02:34 +0530

    Added reduce method to get output

--- puzzle.js
+++ puzzle.js
@@ -1,15 +1,15 @@
 const inputArr = [
-    {
-      a: 1,
-      b: 2
-    },
-    {
-      c: 3,
-      d: 4
-    }
-  ]
-  
-  
+  {
+    a: 1,
+    b: 2,
+  },
+  {
+    c: 3,
+    d: 4,
+  },
+];
+console.log(inputArr.reduce((r, c) => Object.assign(r, c), {}));
+
 //   Output:
 //   {
 //     a: 1,
